0f38286ba3bde8fce14de0026adff772d2ee943c
Alex2034 <fokin.aa@phystech.edu>
2025-04-13 21:33:50 +0300

    minor

--- run_single.sh
+++ run_single.sh
@@ -5,8 +5,10 @@ OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=3 torchrun --standalone --nproc_per_node=
     --data_path "data/shakespeare_char" \
     --device_batch_size 32 \
     --batch_size 32 \
-    --num_iterations 300 \
+    --num_iterations 500 \
     --generate_every 100 \
+    --train_loss_every 20 \
+    --val_loss_every 20 \
     --n_heads 6 \
     --n_layers 6 \
     --head_dim 16 \
