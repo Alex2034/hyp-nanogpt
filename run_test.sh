75a63c9ea8c957e36b7b418725586a20a628d3ff
Alex2034 <2034.fokin.aa@gmail.com>
2025-05-18 11:31:52 +0300

    old lorentz scripts

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=5 torchrun \
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0 torchrun \
     --standalone --nproc_per_node=1 \
     train_gpt2_main.py \
-    --head_mode hyp \
     --test_run > logs/test.txt 2>&1
