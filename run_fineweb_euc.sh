3b56195480107dfc057d9ef15a0c3e165ded3112
Alex2034 <fokin.aa@phystech.edu>
2025-06-02 23:33:17 +0300

    updated loss plotting logic

--- run_fineweb_euc.sh
+++ run_fineweb_euc.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+pids=(38224 38225 38226 38227)
+
+any_pid_running() {
+    for pid in "${pids[@]}"; do
+        if kill -0 "$pid" 2>/dev/null; then
+            return 0  
+        fi
+    done
+    return 1  
+}
+
+
+while any_pid_running; do
+    sleep 5
+done
+
 # SEEDS=(0)
 MODES=('euc')
 SEEDS=(0)
-LR=(1.)
+LR=(10.)
 
 
 for k_lr in "${LR[@]}"; do
     for seed in "${SEEDS[@]}"; do
         for mode in "${MODES[@]}"; do
-            OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0,3 torchrun --standalone --nproc_per_node=2 \
+            OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nproc_per_node=4 \
                 train_gpt2.py \
                 --data_path "data/finewebedu" \
                 --gen_prompt "The " \
                 --device_batch_size 25 \
-                --batch_size 50 \
-                --num_iterations 20001 \
-                --save_every 2000 \
-                --gen_every 2000 \
+                --batch_size 100 \
+                --num_iterations 10001 \
+                --save_every 1000 \
+                --gen_every 1000 \
                 --gen_length 200 \
                 --train_loss_every 50 \
                 --val_loss_every 50 \
@@ -27,7 +43,7 @@ for k_lr in "${LR[@]}"; do
                 --sequence_length 1024 \
                 --attn_mode "$mode" \
                 --head_mode "euc"\
-                --curvature 1. \
+                --curvature 0.001 \
                 --k_lr "$k_lr" \
                 --seed "$seed" \
                 > new_logs/fwe_${mode}_run_${seed}.txt 2>&1
